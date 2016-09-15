bf611b537926ab0c348c195d08007959b5be1abf
boyboi86 <boyboi86@gmail.com>
2016-09-15 14:49:30 +0800

    establish call to server

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,12 @@
+import axios from 'axios';
 
+//The server we created
+const ROOT_URL = 'http://localhost:3000';
 
 export function signinUser({ email, password }){
   return function(dispatch){
     //Submit email/password to server
-
+    axios.post(`${ROOT_URL}`/signin, { email, password })
     //If info is correct, update state to indicate authentication complete
     //save JWT
     //redirect /features
