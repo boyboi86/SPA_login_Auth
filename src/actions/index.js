70abb30c472545de257b8c41dd1b80f47a3f6349
boyboi86 <boyboi86@gmail.com>
2016-09-15 14:23:00 +0800

    update redux-thunk

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+
+
+export function signinUser({ email, password }){
+  return function(dispatch){
+    //Submit email/password to server
+
+    //If info is correct, update state to indicate authentication complete
+    //save JWT
+    //redirect /features
+
+    //If info is incorrect, show error
+  }
+}
