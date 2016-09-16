c2301131e73ab1bff7c0fb14b27c1ea748f614e9
boyboi86 <boyboi86@gmail.com>
2016-09-16 14:31:19 +0800

    update combined reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
 
 //The server we created
 const ROOT_URL = 'http://localhost:3000';
@@ -6,11 +8,16 @@ const ROOT_URL = 'http://localhost:3000';
 export function signinUser({ email, password }){
   return function(dispatch){
     //Submit email/password to server
-    axios.post(`${ROOT_URL}/signin`, { email, password });
-    //If info is correct, update state to indicate authentication complete
-    //save JWT
-    //redirect /features
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then( res => {
+        //If info is correct, update state to indicate authentication complete
+        dispatch({ type: AUTH_USER});
+        //save JWT
+        //redirect /features
+        browserHistory.push('/features');
+      })
+      .catch(function(){
 
-    //If info is incorrect, show error
+      })
   }
 }
