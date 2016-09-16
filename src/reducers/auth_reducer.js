0ff47f9847b89f2ba347be84585d1b402e1fb040
boyboi86 <boyboi86@gmail.com>
2016-09-16 14:33:55 +0800

    auth reducer created

---
+++ src/reducers/auth_reducer.js
@@ -0,0 +1,9 @@
+export default function(state = {}, action){
+  switch(action.type){
+    case AUTH_USER:
+      return { ...state, authenticate: true};
+    case UNAUTH_USER:
+      return { ...state, authenticate: false };
+  }
+  return state;
+}
