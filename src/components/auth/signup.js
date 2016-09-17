23e4fc22542784f31bbf238e05d36e6df8e3a6e4
boyboi86 <boyboi86@gmail.com>
2016-09-17 22:16:00 +0800

    add email validation

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -3,15 +3,20 @@ import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
 class Signup extends Component {
+  handleSubmit(formProps){
+    this.props.signupUser(formProps);
+  }
+
   render(){
 
     const { handleSubmit, fields: {email, password, password_confirm}} = this.props;
 
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.props.handleFormSubmit.bind(this))}>
         <fieldset className="form-group">
           <label>Email: </label>
           <input type="email" className="form-control" { ...email } />
+          { email.touched && email.error && <div className="error">{ email.error }</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Password: </label>
@@ -29,8 +34,11 @@ class Signup extends Component {
 }
 function validate(formProps){
   const error = {}
+  if(!formProps.email){
+    error.email = 'Please provide email';
+  }
 
-  if (formProps.password !== formProps.password_confirm){
+  if (formProps.password !== formProps.password_confirm || typeof (formProps.password || formProps.password_confirm) === 'undefined'){
     error.password_confirm = 'Password must match';
   }
   return error;
