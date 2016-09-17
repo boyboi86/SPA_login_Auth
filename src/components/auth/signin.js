77b51d3634544934ae488bfe011da515b8b28bb5
boyboi86 <boyboi86@gmail.com>
2016-09-17 16:17:45 +0800

    added Link for routing

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -7,6 +7,16 @@ class Signin extends Component{
     this.props.signinUser({ email, password });
   }
 
+  renderAlert(){
+    if(this.props.errMsg){
+      return(
+        <div className="alert alert-danger">
+          { this.props.errMsg }
+        </div>
+      )
+    }
+  }
+
   render(){
     const { handleSubmit, fields: { email, password }} = this.props;
 
@@ -20,12 +30,15 @@ class Signin extends Component{
           <label>Password:</label>
           <input type="password" className="form-control" placeholder="Enter your password" { ...password }/>
         </fieldset>
+        {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign in</button>
       </form>
     )
   }
 }
 
+function mapStateToProps(state){
+  return { errMsg: state.auth.error };
+}
 
-
-export default reduxForm({ form: 'signin', fields: ['email', 'password']}, null, actions )(Signin)
+export default reduxForm({ form: 'signin', fields: ['email', 'password']}, mapStateToProps, actions )(Signin)
