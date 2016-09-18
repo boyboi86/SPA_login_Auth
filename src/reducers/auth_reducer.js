297c268edbdb2c81f7a06a14c75e9781ca240273
boyboi86 <boyboi86@gmail.com>
2016-09-18 22:27:44 +0800

    include fetch msg switch case

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -1,4 +1,4 @@
-import { AUTH_USER, UNAUTH_USER, AUTH_ERR } from '../actions/types';
+import { AUTH_USER, UNAUTH_USER, AUTH_ERR, FETCH_MSG } from '../actions/types';
 
 export default function(state = {}, action){
   switch(action.type){
@@ -8,6 +8,8 @@ export default function(state = {}, action){
       return { ...state, authenticate: false };
     case AUTH_ERR:
       return { ...state, error: action.payload }
+    case FETCH_MSG:
+    return { ...state, msg: action.payload }
   }
   return state;
 }
