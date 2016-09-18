f982c935cc5a005749e179201aa37aea59cfd809
boyboi86 <boyboi86@gmail.com>
2016-09-18 18:54:52 +0800

    privatising route with requireAuth

--- src/reducers/auth_reducer.js
+++ src/reducers/auth_reducer.js
@@ -3,7 +3,7 @@ import { AUTH_USER, UNAUTH_USER, AUTH_ERR } from '../actions/types';
 export default function(state = {}, action){
   switch(action.type){
     case AUTH_USER:
-      return { ...state, authenticate: true};
+      return { ...state, error: '', authenticate: true};
     case UNAUTH_USER:
       return { ...state, authenticate: false };
     case AUTH_ERR:
